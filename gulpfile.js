ba65d448972ece7fac8297c7efd53ffc7d588898
Ricardo Tobias <ricardosantostobias@yahoo.com.br>
2017-06-26 14:49:16 -0300

    - Creating a frontend template - contains Bootstrap, Gulp, SASS

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        concat = require('gulp-concat'),
+        uglify = require('gulp-uglify'),
+        imagemin = require('gulp-imagemin'),
+        pngquant = require('imagemin-pngquant'),
+        gls = require('gulp-live-server');
+
+
+gulp.task('default', ['sass_btsp','sass', 'js_btsp', 'js', 'image', 'watch', 'serve']);
+
+gulp.task('sass_btsp', function () {
+    return gulp.src('public_html/src/vendors/bootstrap/sass/**/*.scss')
+            .pipe(concat('bootstrap.min.css'))
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(gulp.dest('public_html/assets/vendors/bootstrap/css'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('public_html/src/sass/**/*.scss')
+            .pipe(concat('main.min.css'))
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(gulp.dest('public_html/assets/css'));
+});
+
+gulp.task('js_btsp', function () {
+    return gulp.src('public_html/src/vendors/bootstrap/js/**/*.js')
+            .pipe(concat('bootstrap.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('public_html/assets/vendors/bootstrap/js'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('public_html/src/js/**/*.js')
+            .pipe(concat('main.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('public_html/assets/js'));
+});
+
+gulp.task('image', function () {
+    return gulp.src('public_html/src/img/*')
+            .pipe(imagemin({
+                progressive: true,
+                svgoPlugins: [{removeViewBox: false}],
+                use: [pngquant()]
+            }))
+            .pipe(gulp.dest('public_html/assets/img'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('public_html/src/vendors/bootstrap/sass/**/*.scss', ['sass_btsp']);
+    gulp.watch('public_html/src/sass/**/*.scss', ['sass']);
+    gulp.watch('public_html/src/vendors/bootstrap/js/**/*.js', ['js_btsp']);
+    gulp.watch('public_html/src/js/**/*.js', ['js']);
+    gulp.watch('public_html/src/img/*', ['image']);
+});
+gulp.task('serve', function () {
+    var server = gls.static('./', 8000);
+    server.start();
+    gulp.watch('public_html/assets/css/**/*.css', function (file) {
+        gls.notify.apply(server, [file]);
+    });
+    gulp.watch('public_html/assets/js/**/*.js', function (file) {
+        gls.notify.apply(server, [file]);
+    });
+    gulp.watch('public_html/assets/img/*', function (file) {
+        gls.notify.apply(server, [file]);
+    });
+
+});
